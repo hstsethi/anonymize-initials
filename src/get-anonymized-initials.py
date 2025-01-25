50819261f3c844f3d72e94832193fa6bd9005343
Harshit Sethi <hstsethi@outlook.com>
2025-01-25 18:18:45 +0530

    Create get-anonymized-initials.py

---
+++ src/get-anonymized-initials.py
@@ -0,0 +1,17 @@
+import random
+
+
+def genRandomInitial(input_initial: str) -> str:
+    while True:
+        rand_initial = chr(random.randint(65, 90))
+        if rand_initial not in (input_initial, 'I'):
+            return rand_initial
+
+def main():
+    initials = ("H", "S")
+    for initial in initials:
+        anonymized_initial = genRandomInitial(initial)
+        print(initial + ": " + anonymized_initial) # The output will also be a valid YAML
+
+if __name__ == "__main__":
+    main()
